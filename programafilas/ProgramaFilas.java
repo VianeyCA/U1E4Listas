fe8c2a607b49940e2350726d4df0d27fe92f400a
Vianey Ambrosio <71795759+VianeyCA@users.noreply.github.com>
2020-10-05 17:47:36 -0500

    Update ProgramaFilas.java

--- programafilas/ProgramaFilas.java
+++ programafilas/ProgramaFilas.java
@@ -1,30 +1,41 @@
-package programafilas;
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.StringTokenizer;
-
 public class ProgramaFilas {
-
-    public static void main(String[] args) throws FileNotFoundException, IOException {         
-         String a,b;
- 
-      
-         BufferedReader br = new BufferedReader (new FileReader ("C:\\Users\\Graciela\\Desktop\\mate.txt"));
- 
-         a = br.readLine();
- 
-         int numTokens = 0;
-         StringTokenizer st = new StringTokenizer (a);
-
-         while (st.hasMoreTokens())
+    
+     public static void main(String[] args) throws FileNotFoundException, IOException 
+     {         
+         Logica metodo = new Logica(); // creamos un objeto de la clase logica
+         File archivo = null; //Leemos arhivo
+         FileReader fr = null;
+         BufferedReader br = null;
+         List<String> Cadena = new ArrayList<String>(); //Creamos una lista
+         Cadena.add("La materia de estructura es dificil y no se");
+         String texto = "Esto,es,una,prueba";
+         String Linea;
+         String[] palaOr; //Creamos arreglos que almacenan datos
+         String[] palabrasAr;
+         String[] palabrasCompa;
+         archivo = new File("C:\\Users\\Graciela\\Desktop\\doc.txt"); //Buscamos la dirección del archivo
+         fr = new FileReader(archivo);//Instanciamos las variables para leer el archivo
+         br = new BufferedReader(fr);
+         palaOr = texto.split(",");//Metodo split 
+         palabrasAr = Cadena.get(0).split(" ");
+        
+         Linea = br.readLine();//Leemos el archivo de texto
+         palabrasCompa = Linea.split(" ");
+         metodo.verArr(palaOr);//Mostramos cadena 
+         metodo.verArr(palabrasAr);//Mostramos lista
+         metodo.verArr(palabrasCompa);//Mostramos fichero
+         metodo.Compara(palabrasAr, palabrasCompa);//Metodo de comparación
+         try 
          {
-            b = st.nextToken();
-            numTokens++;
-            
-            System.out.println (b);
+             if (null != fr) 
+             {
+                 fr.close();
+             }
          }
-    }
+         catch (Exception e)         
+         {
+            e.printStackTrace();
+         }
+     }
     
 }
